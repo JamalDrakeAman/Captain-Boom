c3c0bfd546edf003502d5529662428a1e189d07f
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-11-06 21:08:43 +0100

    level1

---
+++ level/level1.js
@@ -0,0 +1,46 @@
+const level1 = new Level(
+    [
+        new Chicken(),
+        new Chicken(),
+        new Chicken()
+    ],
+    [
+        new Cloud()
+    ],
+    [
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', -719),
+
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', 0),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', 0),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', 0),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 0),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', 0),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', 0),
+
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', 719),
+
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', 719 * 2),
+
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', 719 * 3),
+    ]
+);
--- models/world.class.js
+++ models/world.class.js
@@ -1,54 +1,11 @@
 class World {
     character = new Character();
 
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
+    enemies = level1.enemies;
 
-    clouds = [
-        new Cloud()
-    ];
+    clouds = level1.clouds;
 
-    backgroundObjects = [
-        new BackgroundObject('imgs/game_background_4/layers/sky.png', -719),
-        new BackgroundObject('imgs/game_background_4/layers/sun.png', -719),
-        new BackgroundObject('imgs/game_background_4/layers/sea.png', -719),
-        new BackgroundObject('imgs/game_background_4/layers/cloud.png', -719),
-        new BackgroundObject('imgs/game_background_4/layers/decor.png', -719),
-        new BackgroundObject('imgs/game_background_4/layers/land.png', -719),
-
-        new BackgroundObject('imgs/game_background_4/layers/sky.png', 0),
-        new BackgroundObject('imgs/game_background_4/layers/sun.png', 0),
-        new BackgroundObject('imgs/game_background_4/layers/sea.png', 0),
-        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 0),
-        new BackgroundObject('imgs/game_background_4/layers/decor.png', 0),
-        new BackgroundObject('imgs/game_background_4/layers/land.png', 0),
-
-        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719),
-        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719),
-        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719),
-        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719),
-        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719),
-        new BackgroundObject('imgs/game_background_4/layers/land.png', 719),
-
-        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719 * 2),
-        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719 * 2),
-        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719 * 2),
-        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719 * 2),
-        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719 * 2),
-        new BackgroundObject('imgs/game_background_4/layers/land.png', 719 * 2),
-
-        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719 * 3),
-        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719 * 3),
-        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719 * 3),
-        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719 * 3),
-        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719 * 3),
-        new BackgroundObject('imgs/game_background_4/layers/land.png', 719 * 3),
-
-
-    ];
+    backgroundObjects = level1.backgroundObjects;
 
     canvas;
     ctx;
