8a94697bc4ec1fefa5a46d6b4f30b3b4a8429b9c
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-11-06 20:21:08 +0100

    update background map

--- models/world.class.js
+++ models/world.class.js
@@ -12,14 +12,42 @@ class World {
     ];
 
     backgroundObjects = [
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', -719),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', -719),
+
         new BackgroundObject('imgs/game_background_4/layers/sky.png', 0),
         new BackgroundObject('imgs/game_background_4/layers/sun.png', 0),
         new BackgroundObject('imgs/game_background_4/layers/sea.png', 0),
         new BackgroundObject('imgs/game_background_4/layers/cloud.png', 0),
         new BackgroundObject('imgs/game_background_4/layers/decor.png', 0),
         new BackgroundObject('imgs/game_background_4/layers/land.png', 0),
-        
-        
+
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', 719),
+
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719 * 2),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', 719 * 2),
+
+        new BackgroundObject('imgs/game_background_4/layers/sky.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/sun.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/sea.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/cloud.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/decor.png', 719 * 3),
+        new BackgroundObject('imgs/game_background_4/layers/land.png', 719 * 3),
+
+
     ];
 
     canvas;
