26fbe50943d1cc680e258576473f171929c9e018
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-11-07 20:29:08 +0100

    endboss animate

---
+++ models/endboss.class.js
@@ -0,0 +1,34 @@
+class Endboss extends MovableObject {
+
+    height = 350;
+    width = 350;
+    y = 50;
+
+    IMAGES_WALKING = [
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_1.png',
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_2.png',
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_3.png',
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_4.png',
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_5.png',
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_6.png',
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_7.png',
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_8.png',
+        'img/4_boss/2_alert/Bringer-of-Death_Cast_9.png'
+    ];
+
+    constructor() {
+        super().loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+
+        this.x = 2200;
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
+    }
+
+
+}
