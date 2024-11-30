74c5fc5620f5db0475372fe9a0d159cf4a17f170
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-11-30 15:21:08 +0100

    fullscreen update

--- js/game.js
+++ js/game.js
@@ -12,46 +12,23 @@ function init() {
 
 function startGame() {
     let startScreen = document.getElementById('startscreen');
-    startScreen.classList.add('d-none')
+    startScreen.classList.add('d-none');
 }
 
 /////////////////////
 
 function fullscreen() {
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-    // canvas.requestFullscreen();
-
     let canvas = document.getElementById('canvas');
-    canvas.classList.toggle('fullscreen')
+    let gameTitle = document.getElementById('game-title');
+
+
+    canvas.classList.toggle('fullscreen');
+
+    gameTitle.classList.toggle('d-none');
+
 }
 
-
-
-// function fullscreen() {
-//     let fullscreen = document.getElementById('fullscreen');
-//     enterFullscreen(fullscreen);
-// }
-
-// function enterFullscreen(element) {
-//     if (element.requestFullscreen) {
-//         element.requestFullscreen();
-//     } else if (element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
-//         element.msRequestFullscreen();
-//     } else if (element.webkitRequestFullscreen) {  // iOS Safari
-//         element.webkitRequestFullscreen();
-//     }
-// }
-
-// function exitFullscreen() {
-//     if (document.exitFullscreen) {
-//         document.exitFullscreen();
-//     } else if (document.webkitExitFullscreen) {
-//         document.webkitExitFullscreen();
-//     }
-// }
-
-///////////////////////////
+////////////////////
 
 window.addEventListener('keydown', (e) => {
     if (e.keyCode == 39) {
