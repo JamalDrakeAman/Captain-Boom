547d8e8b729bbefd235322dd60726a3e8e18ac5b
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-12-03 23:21:12 +0100

    update endboss images

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -11,6 +11,38 @@ class Endboss extends MovableObject {
     }
 
     IMAGES_WALKING = [
+        'img/4_boss/walk/Bringer-of-Death_Walk_1.png',
+        'img/4_boss/walk/Bringer-of-Death_Walk_2.png',
+        'img/4_boss/walk/Bringer-of-Death_Walk_3.png',
+        'img/4_boss/walk/Bringer-of-Death_Walk_4.png',
+        'img/4_boss/walk/Bringer-of-Death_Walk_5.png',
+        'img/4_boss/walk/Bringer-of-Death_Walk_6.png',
+        'img/4_boss/walk/Bringer-of-Death_Walk_7.png',
+        'img/4_boss/walk/Bringer-of-Death_Walk_8.png',
+
+    ];
+
+    IMAGES_ATTACK = [
+        'img/4_boss/attack/Bringer-of-Death_Attack_1.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_2.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_3.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_4.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_5.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_6.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_7.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_8.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_9.png',
+        'img/4_boss/attack/Bringer-of-Death_Attack_10.png'
+    ];
+
+    IMAGES_HURT = [
+        'img/4_boss/hurt/Bringer-of-Death_Hurt_1.png',
+        'img/4_boss/hurt/Bringer-of-Death_Hurt_2.png',
+        'img/4_boss/hurt/Bringer-of-Death_Hurt_3.png'
+    ];
+
+
+    IMAGES_ALERT = [
         'img/4_boss/2_alert/Bringer-of-Death_Cast_1.png',
         'img/4_boss/2_alert/Bringer-of-Death_Cast_2.png',
         'img/4_boss/2_alert/Bringer-of-Death_Cast_3.png',
@@ -22,9 +54,23 @@ class Endboss extends MovableObject {
         'img/4_boss/2_alert/Bringer-of-Death_Cast_9.png'
     ];
 
+    IMAGES_DEATH = [
+        'img/4_boss/5_dead/Bringer-of-Death_Death_1.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_2.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_3.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_4.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_5.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_6.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_7.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_8.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_9.png',
+        'img/4_boss/5_dead/Bringer-of-Death_Death_10.png'
+
+    ];
+
     constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
-        this.loadImages(this.IMAGES_WALKING);
+        super().loadImage(this.IMAGES_HURT[0]);
+        this.loadImages(this.IMAGES_HURT);
         this.animate();
 
         this.x = 2200;
@@ -32,7 +78,7 @@ class Endboss extends MovableObject {
 
     animate() {
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+            this.playAnimation(this.IMAGES_HURT);
         }, 200);
     }
 
