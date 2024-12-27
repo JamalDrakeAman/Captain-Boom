74386824c56ebd33d61601a2cb2b00e8e5a85a0f
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-12-28 00:22:46 +0100

    level js docs

--- models/level.class.js
+++ models/level.class.js
@@ -1,9 +1,20 @@
+/**
+ * Represents a game level, containing enemies, clouds, and background objects.
+ */
 class Level {
     enemies;
     clouds;
     backgroundObjects;
     level_end_x = 4250;
 
+
+    /**
+     * Constructs a new `Level` instance with specified enemies, clouds, and background objects.
+     * 
+     * @param {EnemyObject[]} enemies - The enemies to include in the level.
+     * @param {Cloud[]} clouds - The clouds to include in the level.
+     * @param {BackgroundObject[]} backgroundObjects - The background objects to include in the level.
+     */
     constructor(enemies, clouds, backgroundObjects) {
         this.enemies = enemies;
         this.clouds = clouds;
