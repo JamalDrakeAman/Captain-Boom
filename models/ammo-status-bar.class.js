0c6bd8ad5dfffea09192367d26d8fb1fe6202506
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-12-28 10:30:39 +0100

    ammo statusBar class js docs

--- models/ammo-status-bar.class.js
+++ models/ammo-status-bar.class.js
@@ -1,7 +1,16 @@
+/**
+ * Represents the ammo status bar displayed in the game.
+ * Inherits from the StatusBar class and tracks the player's ammo count.
+ */
 class AmmoStatusBar extends StatusBar {
 
     itemCount = 10;
 
+
+    /**
+     * Initializes a new instance of the AmmoStatusBar class.
+     * Loads the ammo status bar image and sets its initial position and dimensions.
+     */
     constructor() {
         super();
         this.loadImage('img/8_item/ammo/bullet0.png');
@@ -11,6 +20,11 @@ class AmmoStatusBar extends StatusBar {
         this.height = 110;
     }
 
+
+     /**
+     * Increases the ammo count by one.
+     * This method is called when the player collects an ammo item.
+     */
     pickupItem() {
         this.itemCount += 1;
     }
